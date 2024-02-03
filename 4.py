7d89032f43efe2a79731bcfd495af8c50567811b
Ultard <glebpozbnakov62@gmail.com>
2024-02-03 13:59:48 +0000

    feat: решена 4 задача

--- 4.py
+++ 4.py
@@ -1,17 +1,44 @@
 import csv
+import random
+import string
 
-def generate_password(username):
+
+def create_initials(s):
     """
-    Генерирует логин пароль для пользователя
+    Эта функция, которая создает логин на основе имени
 
     Параметры:
-    username - имя пользователя
+    s - имя пользователя
 
     Возвращает:
-    пароль для пользователя
+    строка - логин на основе имени
     """
+    names = s.split()
+    return f'{names[0]}_{names[1][0]}{names[2][0]}'
 
+
+def create_password():
+    """
+    Эта функция, которая создает случайный пароль
+
+    Возвращает:
+    строку - случайный пароль
+    """
+    characters = string.ascii_letters + string.digits
+    password = ''.join(random.choice(characters) for _ in range(10))
+    return password
+
+
+students_passwords = []
 with open('scientist.txt', 'r', encoding="utf") as csvfile:
-    reader = csv.DictReader(csvfile, delimiter='#')
-    reader = [row for row in reader]
+    reader = list(csv.DictReader(csvfile, delimiter='#'))
 
+    for row in reader:
+        row['login'] = create_initials(row['ScientistName'])
+        row['password'] = create_password()
+        students_passwords.append(row)
+
+with open('scientist_password.csv', 'w', newline='', encoding="utf") as file:
+    writer = csv.DictWriter(file, delimiter='#', fieldnames=['ScientistName', 'preparation', 'date', 'components', 'login', 'password'])
+    writer.writeheader()
+    writer.writerows(students_passwords)
